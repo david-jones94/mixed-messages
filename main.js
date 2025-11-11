9e419da69e3a42fd7265c0f39081a886b9832881
David Jones <david.jones94@live.co.uk>
2025-11-11 19:57:42 +0000

    Code Formatting - no logic change

--- main.js
+++ main.js
@@ -3,7 +3,6 @@
 import { dishes } from './data.js';
 import { filterArr, randomiser } from './utils.js';
 
-
 // Generate Random Order Number
 const orderNo = Math.floor(Math.random() * 999);
     
@@ -13,7 +12,6 @@ const diets = [...new Set(dishes.map(dish => dish.dietary))];
 const userInput = randomiser(diets);
 
 // New Filtering: Reduce dishes based on dietary preference
-
 const groups = dishes.reduce((acc, dish) => {
     if (!acc[dish.dietary]) acc[dish.dietary] = {};
     if (!acc[dish.dietary][dish.dish]) acc[dish.dietary][dish.dish] = [];
@@ -25,28 +23,14 @@ const groups = dishes.reduce((acc, dish) => {
 const userDishes = groups[userInput];
 
 // Group Filtered Dishes by Course
-
 const starters = groups[userInput]['Starter'];
 const mains = groups[userInput]['Main'];
-const desserts = groups[userInput]?.Dessert ? groups[userInput]['Dessert'] : groups['Vegan']['Dessert'];
-const veDesserts = groups['Vegan']?.Dessert || [];
+const desserts = groups[userInput]?.Dessert ? groups[userInput]['Dessert'] : groups['Vegan']['Dessert'] || [];
 
 // Random Dish Selection
 const starter = randomiser(starters);
 const main = randomiser(mains);
 const dessert = randomiser(desserts);
-const veDessert = randomiser(veDesserts);
-
-// Function to select Vegan/non-Vegan dessert
-const randDesserts = () => {
-          
-    if (userInput === 'Vegan') {
-        return veDessert;
-    } else {
-        return dessert;
-    };
-}
-
 
 // Script Output
 console.log(`Order No: ${orderNo}`);
