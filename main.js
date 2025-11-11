114f2827a26484fdd706093b115977a0d69c2725
David Jones <david.jones94@live.co.uk>
2025-11-11 11:43:22 +0000

    Moved Helper Functions and condensed repeated code

--- main.js
+++ main.js
@@ -3,27 +3,46 @@
 import { dishes } from './data.js';
 
 
-    const orderNo = Math.floor(Math.random() * 999);
+// Helper Functions
+const filterArr = (arr, key, value) => {return arr.filter(obj => obj[key] === value);}
+const randomiser = (arr) => {return arr[Math.floor(Math.random() * arr.length)];}
+
+
+const orderNo = Math.floor(Math.random() * 999);
     
-    // User Dietary Preference
-    const diets = [...new Set(dishes.map(dish => dish.dietary))];
-    const userInput = diets[Math.floor(Math.random() * diets.length)];
+// User Dietary Preference
 
-    // Filter Dishes Based on Dietary Preference
-    const userDishes = dishes.filter(dish => dish.dietary === userInput);
-    const starter = userDishes.filter(dish => dish.dish === 'Starter');
-    const main = userDishes.filter(dish => dish.dish === 'Main');
-    const dessert = dishes.filter(dish => dish.dish === 'Dessert');
-
-    // Random Selection
-    const randomStarter = starter[Math.floor(Math.random() * starter.length)];
-    const randomMain = main[Math.floor(Math.random() * main.length)];
-    const randomDessert = dessert[Math.floor(Math.random() * dessert.length)];
+const diets = [...new Set(dishes.map(dish => dish.dietary))];
+const userInput = randomiser(diets);
 
 
-    // Output
-    console.log(`Order No: ${orderNo}`);
-    console.log(`Dietary Preference: ${userInput}`);
-    console.log(`Starter: ${randomStarter.name} (${randomStarter.cuisine})`);
-    console.log(`Main: ${randomMain.name} (${randomMain.cuisine})`);
-    console.log(`Dessert: ${randomDessert.name} (${randomDessert.cuisine})`);
+// Filter Dishes Based on Dietary Preference
+const userDishes = filterArr(dishes, 'dietary', userInput);
+const starters = filterArr(userDishes, 'dish', 'Starter');
+const mains = filterArr(userDishes, 'dish', 'Main');
+const desserts = filterArr(dishes, 'dish', 'Dessert');
+const veDesserts = filterArr(userDishes, 'dish', 'Dessert');
+
+// Random Selection
+const starter = randomiser(starters);
+const main = randomiser(mains);
+const dessert = randomiser(desserts);
+const veDessert = randomiser(veDesserts);
+
+
+const randDesserts = () => {
+          
+    if (userInput === 'Vegan') {
+        return veDessert;
+    } else {
+        return dessert;
+    };
+}
+
+
+// Script Output
+console.log(`Order No: ${orderNo}`);
+console.log(`Dietary Preference: ${userInput}`);
+console.log(`Starter: ${starter.name} (${starter.cuisine})`);
+console.log(`Main: ${main.name} (${main.cuisine})`);
+console.log(`Dessert: ${randDesserts().name} (${randDesserts().cuisine})`);
